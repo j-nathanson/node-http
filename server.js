a3e2b1d00bc0b44afdf062023e29f9da4a82d480
jacobnathanson <jacobnath93@gmail.com>
2022-02-23 12:32:26 -0500

    comments

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+// import the core http module from Node.js to allow Node.js to transfer data over Hyper Text Transfer Protocol (HTTP).
 const http = require('http');
 
+// set hostname and port number
+// domain name assigned to a host computer
 const hostname = 'localhost';
 const port = 3000;
+
+// The http.createServer() method turns your computer into an HTTP server, by creating a server object
+//Request Header: This type of headers contains information about the fetched request by the client.
+// set status code to 200 which is ok
+// set the response header with a name and value
+// Calling the writable.end() method signals that no more data will be written to the Writable. The optional chunk and encoding arguments allow one final additional chunk of data to be written immediately before closing the stream.
 
 const server = http.createServer((req, res) => {
     console.log(req.headers);
